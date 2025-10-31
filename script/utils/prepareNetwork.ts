6a0e43ad4b1d5cd4195069ed0417ba5c722e133e
Myron Rotter <myronrotter01@t-online.de>
2025-10-31 15:49:18 +0100

    feat: add taiko hoodi testnet

--- script/utils/prepareNetwork.ts
+++ script/utils/prepareNetwork.ts
@@ -18,6 +18,7 @@ export enum NETWORK {
   WORLDCHAIN = 'worldchain-mainnet',
   ZKSYNC_ERA = 'zksync-era',
   ZORA = 'zora-mainnet',
+  TAIKO_HOODI_TESTNET = 'taiko-hoodi-testnet',
 }
 
 export enum SUBGRAPH_TYPE {
